40adb705215e0669a933089b6e6f8bf2e7f0a8e9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-14 15:07:22 -0700

    made progress with discord client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,44 @@
-import express, { Application, Request, Response } from 'express'
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import express, { Application, Request, Response } from 'express';
+import { readdirSync } from 'fs';
+import dotenv from 'dotenv';
+import { join } from 'path';
+dotenv.config();
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ],
+});
+
+const port: number = Number(process.env.PORT) || 5000;
 const app: Application = express();
+client.commands = new Collection();
+app.set('discordClient', client);
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('hello world')
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = join(commandsPath, file);
+	const command = require(filePath);
+
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+app.get('/', (req: Request, res: Response): void => {
+    res.status(200).send('hello world!');
 })
 
-const port = process.env.PORT || 5000
+client.on("ready", async (): Promise<void> => {
+	client.user.setActivity('hello world!');
 
-app.listen(port, () => console.log(`listening on port ${port}...`))
+	console.log('bot is ready...');
+});
+
+app.listen(port, (): void => console.log(`listening on port ${port}...`));
+client.login(process.env.TOKEN);
