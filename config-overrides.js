cf0908ecc8115ba753b98b72eab1518e319f290e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-14 04:33:34 -0700

    updated config overrides to match new src

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
     paths: function (paths, env) {
-        paths.appHtml = path.resolve(__dirname, 'client/public/index.html')
-        paths.appIndexJs = path.resolve(__dirname, 'client/src/index.js')
-        paths.appPublic = path.resolve(__dirname, 'client/public')
-        paths.appBuild = path.resolve(__dirname, 'client/build')
-        paths.appSrc = path.resolve(__dirname, 'client/src')
+        paths.appHtml = path.resolve(__dirname, 'src/client/public/index.html')
+        paths.appIndexJs = path.resolve(__dirname, 'src/client/src/index.js')
+        paths.appPublic = path.resolve(__dirname, 'src/client/public')
+        paths.appBuild = path.resolve(__dirname, 'src/client/build')
+        paths.appSrc = path.resolve(__dirname, 'src/client/src')
         return paths;
     }
 };
