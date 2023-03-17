4242d0a77b156832124b1918b29adea5ba185392
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-16 23:06:59 -0700

    created errors index file

---
+++ src/server/errors/index.ts
@@ -0,0 +1,13 @@
+import BadRequestError from "./bad-request";
+import CustomError from "./custom-error";
+import ForbiddenError from "./forbidden";
+import NotFoundError from "./not-found";
+import UnauthorizedError from "./unauthorized";
+
+export {
+    BadRequestError,
+    CustomError,
+    ForbiddenError,
+    NotFoundError,
+    UnauthorizedError
+}
