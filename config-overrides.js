57c245217af983d7b58485ef80b264bdc58b6a4f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-18 13:38:14 -0700

    changed build location

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ module.exports = {
         paths.appHtml = path.resolve(__dirname, 'src/client/public/index.html')
         paths.appIndexJs = path.resolve(__dirname, 'src/client/src/index.js')
         paths.appPublic = path.resolve(__dirname, 'src/client/public')
-        paths.appBuild = path.resolve(__dirname, 'src/client/build')
+        paths.appBuild = path.resolve(__dirname, 'dist/client/build')
         paths.appSrc = path.resolve(__dirname, 'src/client/src')
         return paths;
     }
