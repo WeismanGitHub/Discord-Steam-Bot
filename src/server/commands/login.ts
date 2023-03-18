0e9cdce48fbcdcdb38b0395772b37514515da2d0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-18 14:27:57 -0700

    changed test to login command

--- src/server/commands/login.ts
+++ src/server/commands/login.ts
@@ -2,10 +2,10 @@ import { SlashCommandBuilder, Interaction } from 'discord.js'
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('test')
-		.setDescription('test command')
+		.setName('login')
+		.setDescription('Connect your Steam account to this Discord Bot.')
 	,
 	async execute(interaction: Interaction): Promise<void> {
-        console.log(interaction)
+        console.log(interaction.user)
 	},
 };
