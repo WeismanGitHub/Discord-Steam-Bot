8af1d4abea4a56e4be14745cd6d48aef000cde0e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-17 22:26:54 -0700

    use express packages

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from 'express';
 import { ActivityType, GatewayIntentBits } from 'discord.js';
 import { CustomClient } from './custom-client';
+import rateLimit from 'express-rate-limit'
+import compression from 'compression'
+require('express-async-errors')
+import helmet from 'helmet'
+import cors from 'cors'
 
 const client: CustomClient = new CustomClient({
     intents: [
@@ -9,6 +14,7 @@ const client: CustomClient = new CustomClient({
 });
 
 const port: number = Number(process.env.PORT) || 5000;
+const max = 15
 
 const app: Application = express();
 app.set('discordClient', client);
@@ -21,5 +27,31 @@ client.on('ready', () => {
 	console.log('bot is ready...')
     client.setPresence(ActivityType.Playing, 'on Steam')
 })
+
+const limiter = rateLimit({
+    windowMs: 1000,
+	max: max,
+	standardHeaders: true,
+	legacyHeaders: false,
+	message: `Rate Limit: ${max} requests per second`
+})
+
+app.use(helmet({
+	contentSecurityPolicy: {
+		directives: {
+			...helmet.contentSecurityPolicy.getDefaultDirectives(),
+			"img-src": ["'self'"],
+			'media-src': ["'self'"],
+			"default-src": ["'self'"]
+		},
+	},
+	crossOriginEmbedderPolicy: false
+}))
+
+app.use(limiter)
+app.use(compression())
+app.use(cors({ origin: ['http://localhost:5000'] }))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.listen(port, (): void => console.log(`listening on port ${port}...`));
