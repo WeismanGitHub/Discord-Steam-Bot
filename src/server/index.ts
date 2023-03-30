de67bd604e227dfe0991b09f222c6302551f8ea1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-30 16:27:03 -0700

    added routes and changed a couple minor things

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,16 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { ActivityType, GatewayIntentBits } from 'discord.js';
 import InternalServerError from './errors/internal-server';
+import { CustomError, NotFoundError } from './errors';
 import { CustomClient } from './custom-client';
 import rateLimit from 'express-rate-limit';
 import fetchMetadata from 'fetch-metadata';
 import authRouter from './routers/auth';
-import { CustomError } from './errors';
 import compression from 'compression'
 require('express-async-errors')
 import helmet from 'helmet'
 import cors from 'cors'
+import path from 'path'
 
 const client: CustomClient = new CustomClient({
     intents: [
@@ -17,16 +18,11 @@ const client: CustomClient = new CustomClient({
     ],
 });
 
-const port: number = Number(process.env.PORT) || 5000;
-const max: number = 15
-
 const app: Application = express();
 app.set('discordClient', client);
 
-client.on('ready', (): void => {
-	console.log('bot is ready...')
-    client.setPresence(ActivityType.Playing, 'on Steam')
-})
+const port: number = Number(process.env.PORT) || 5000;
+const max: number = 15
 
 const limiter = rateLimit({
     windowMs: 1000,
@@ -69,6 +65,14 @@ app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
 
+app.use('/api/*', (req: Request, res: Response, next: NextFunction): void => {
+	throw new NotFoundError('Route does not exist.')
+})
+
+app.get('/*', (req, res) => {
+	res.status(200).sendFile(path.resolve(__dirname, '../client/build/index.html'))
+})
+
 app.get('/links/discord', (req: Request, res: Response): void => {
 	const redirectURL: string | undefined = process.env.DISCORD_OAUTH_URL
 
@@ -79,9 +83,14 @@ app.get('/links/discord', (req: Request, res: Response): void => {
 	res.status(200).redirect(redirectURL)
 })
 
-app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction): void => {
     console.error(err.message)
     res.status(err.statusCode || 500).send(err.message)
+})
+
+client.on('ready', (): void => {
+	console.log('bot is ready...')
+    client.setPresence(ActivityType.Playing, 'something')
 })
 
 app.listen(port, (): void => console.log(`listening on port ${port}...`));
