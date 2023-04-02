0a8f0ec2669bd4fb846f96c8e8dbeab40f18e9f1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-01 21:33:56 -0700

    made progress with implementing state

--- src/client/src/elements/discord-auth.js
+++ src/client/src/elements/discord-auth.js
@@ -2,6 +2,17 @@ import { useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import axios, * as others from 'axios'
 
+function generateRandomString() {
+	let randomString = '';
+	const randomNumber = Math.floor(Math.random() * 10);
+
+	for (let i = 0; i < 20 + randomNumber; i++) {
+		randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94));
+	}
+
+	return randomString;
+}
+
 export default function DiscordAuth() {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [authorized, setAuthorized] = useState(false)
@@ -11,8 +22,11 @@ export default function DiscordAuth() {
 		const state = searchParams.get('state')
 		setSearchParams({})
 
-		if (!code) {
-			return
+		if (!code || localStorage.getItem('oauth-state') !== state) {
+			const randomString = generateRandomString();
+			
+			localStorage.setItem('oauth-state', randomString);
+			return setSearchParams({ state: randomString })
 		}
 
 		axios.post('/api/v1/auth/discord', { code })
