096bdb8a238843878602333088d82bd53d45746d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-01 20:39:49 -0700

    sends code to backend

--- src/client/src/elements/discord-auth.js
+++ src/client/src/elements/discord-auth.js
@@ -1,28 +1,33 @@
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios, * as others from 'axios'
 
 export default function DiscordAuth() {
-  return (
-    <div>
-        <title>Discord auth</title>
-        <meta name="og:title" content="Discord auth"/>
-        <meta name="theme-color" content="#39e66a" />
-            <h2>Discord auth</h2>
-            <h3>Lol my discord auth thing idk</h3>
-            <div style={{ marginTop: '8px'}}>
-            <button
-                href={`http://localhost:3001/auth/discord/login`}
-                type="primary"
-                block
-                size="large"
-                style={{
-                    marginTop: '10px',
-                    backgroundColor: '#7289DA',
-                    border: 'none',
-                }}
-            >
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [authorized, setAuthorized] = useState(false)
 
-            Login with Discord 
-            </button>
-        </div>
-    </div>
-  );
+    useEffect(() => {
+		const code = searchParams.get('code')
+		const state = searchParams.get('state')
+		setSearchParams({})
+
+		if (!code) {
+			return
+		}
+
+		axios.post('/api/v1/auth/discord', { code })
+		.then(res => {
+			setAuthorized(true)
+			console.log(res)
+		})
+		.catch(console.error);
+    }, [])
+    
+	if (authorized) {
+		return <div>Authorized</div>
+	} else {
+		return <a href={process.env.REACT_APP_DISCORD_OAUTH_URL}>
+			<button>Authorize</button>
+    	</a>
+	}
 }
