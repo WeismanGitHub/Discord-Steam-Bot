9d66a93e2339cecc6550a3f84d300d034b691e85
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-04 06:58:20 -0700

    created configuration interface

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+interface configuration {
+    // Discord
+    discordToken: undefined | string
+    testGuildID: undefined | string
+    discordClientID: undefined | string
+    discordClientSecret: undefined | string
+    mainAccountID: undefined | string
+    discordOAuthURL: undefined | string
+    discordStatus: string
+    
+    // App
+    appPort: number
+    jwtSecret: undefined | string
+    jwtLifetime: string
+    redirectURI: undefined | string
+    
+    // POSTGRESQL
+    postgresPassword: undefined | string
+    postgresPort: number
+    postgresUser: undefined | string
+    postgresHost: undefined | string
+
+    // Rate Limiter
+    windowMs: number
+    max: number
+    message: string
+    standardHeaders: boolean
+    legacyHeaders: boolean,
+}
