0a0d1023d311f0e527eefa8bd056f126f00fb015
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-06 05:37:54 -0700

    added internal server error

--- src/server/errors/index.ts
+++ src/server/errors/index.ts
@@ -3,11 +3,13 @@ import CustomError from "./custom-error";
 import ForbiddenError from "./forbidden";
 import NotFoundError from "./not-found";
 import UnauthorizedError from "./unauthorized";
+import InternalServerError from "./internal-server";
 
 export {
     BadRequestError,
     CustomError,
     ForbiddenError,
     NotFoundError,
-    UnauthorizedError
+    UnauthorizedError,
+    InternalServerError
 }
