bf028c9e43356cd5c0f94a503a26a4ad041e7ab8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-07 00:29:45 -0700

    moved authorize command into help

--- src/server/commands/help.ts
+++ src/server/commands/help.ts
@@ -1,7 +1,13 @@
 import {
 	SlashCommandBuilder,
 	CommandInteraction,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
 } from 'discord.js'
+
+import { config } from '../../config'
 
 export default {
 	data: new SlashCommandBuilder()
@@ -9,5 +15,23 @@ export default {
 		.setDescription("Information about this bot.")
 	,
 	async execute(interaction: CommandInteraction): Promise<void> {
+		const embed: EmbedBuilder = new EmbedBuilder()
+		.setColor('#8F00FF') // Purple
+		.setDescription("Click the link to allow this bot to save your Steam connection. Use the `/unauthorize` command to delete all your data stored by the bot.")
+		.addFields({ name: 'Contact the Creator:', value: `<@${config.mainAccountID}>` })
+	
+		const row = new ActionRowBuilder<ButtonBuilder>()
+		.addComponents([
+			new ButtonBuilder()
+			.setLabel('Authorization')
+			.setURL(config.redirectURI!)
+			.setStyle(ButtonStyle.Link),
+		])
+
+		interaction.reply({
+			embeds: [embed],
+			components: [row],
+			ephemeral: true
+		})
 	}
 }
