be6b38407f3b4ecab5a81e446ac42b91591398f0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-07 21:58:39 -0700

    finished unauthorize command

--- src/server/commands/unauthorize.ts
+++ src/server/commands/unauthorize.ts
@@ -1,13 +1,24 @@
-import {
-	SlashCommandBuilder,
-	CommandInteraction,
-} from 'discord.js'
+import { SlashCommandBuilder, CommandInteraction, EmbedBuilder } from 'discord.js'
+import { BadRequestError, InternalServerError } from '../errors';
+import { UserModel } from '../db/models';
 
 export default {
 	data: new SlashCommandBuilder()
 		.setName('unauthorize')
-		.setDescription('Delete all your data stored by this bot.')
+		.setDescription('Delete all your stored data.')
 	,
 	async execute(interaction: CommandInteraction): Promise<void> {
+		const res = await UserModel.deleteOne({ _id: interaction.user.id })
+		.catch(err => { throw new InternalServerError('Could not delete your data.') })
+
+		if (!res.deletedCount) {
+			throw new BadRequestError("Nothing was changed. Maybe this bot already wasn't authorized?")
+		}
+
+		interaction.reply({ embeds: [
+			new EmbedBuilder()
+            .setTitle('Your data has been deleted!')
+            .setColor('#8F00FF') // Purple
+		]})
 	}
 }
