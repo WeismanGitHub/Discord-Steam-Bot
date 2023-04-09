706bf152abbef401f30bc9c4c8f8007f8d6a40dc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-09 02:35:07 -0700

    created titleEmbed

--- src/server/utils/embeds.ts
+++ src/server/utils/embeds.ts
@@ -4,8 +4,16 @@ function errorEmbed(description: string | null): EmbedBuilder {
     return new EmbedBuilder()
         .setTitle("There's been an error!")
         .setDescription(description)
-        .setColor('#FF0000')
+        .setColor('#FF0000') // Red
 }
+
+function titleEmbed(title: string): EmbedBuilder {
+    return new EmbedBuilder()
+        .setTitle(title)
+        .setColor('#8F00FF') // Purple
+}
+
 export {
-    errorEmbed
+    errorEmbed,
+    titleEmbed,
 }
