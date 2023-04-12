5c4b4c3f33f156101a6f54ae161721de27f646f3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-12 16:08:37 -0700

    created

---
+++ src/client/src/elements/admin.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios, * as others from 'axios'
+import { errorToast } from './toasts'
+
+export default function Admin() {
+	const [authorized, setAuthorized] = useState(false)
+
+    useEffect(async () => {
+		const [guilds, users] = await Promise.all([
+			axios.get('/api/v1.0.0/admin/guilds'),
+			axios.get('/api/v1.0.0/admin/users')
+		])
+		.then(res => {
+			setAuthorized(true)
+		})
+		.catch(err => {
+			setAuthorized(false)
+			errorToast(err.response.data.error || err.message)
+		});
+
+		console.log(guilds, users)
+    }, [])
+    
+	if (!authorized) {
+		return <h2>You're not allowed here!</h2>
+	} else {
+		return <h2>Authorized</h2>
+	}
+}
