f94c0610d4014f8b415144fc369e1a992593f76e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-13 07:37:32 -0700

    added users route

--- src/server/api/v1/routers/admin.ts
+++ src/server/api/v1/routers/admin.ts
@@ -1,8 +1,9 @@
-import { getBotGuilds } from '../controllers'
+import { getBotGuilds, getUsers } from '../controllers'
 import { Router } from 'express';
 
 const adminRouter: Router = Router();
 
 adminRouter.route('/guilds').get(getBotGuilds)
+adminRouter.route('/users').get(getUsers)
 
 export default adminRouter
