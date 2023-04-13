c4cf01acad1ed9e75c235fb5ec42e9fdc31ba3b2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-13 09:03:46 -0700

    added logout + unauthorize routes

--- src/server/api/v1/routers/auth.ts
+++ src/server/api/v1/routers/auth.ts
@@ -1,8 +1,13 @@
-import { discordAuth } from '../controllers'
+import { discordAuth, logout, unauthorize } from '../controllers'
+import { userAuth } from '../middleware';
 import { Router } from 'express';
 
 const authRouter: Router = Router();
 
+authRouter.use('/unauthorize', userAuth)
+
 authRouter.route('/discord').post(discordAuth)
+authRouter.route('/logout').get(logout)
+authRouter.route('/unauthorize').get(unauthorize)
 
 export default authRouter
