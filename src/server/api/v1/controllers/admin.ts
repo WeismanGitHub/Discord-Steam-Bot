04c6e9a75363799299348fb7e1389906f05fc6c7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-13 07:29:14 -0700

    finished getBotGuilds

--- src/server/api/v1/controllers/admin.ts
+++ src/server/api/v1/controllers/admin.ts
@@ -1,11 +1,22 @@
-// import { BadRequestError, InternalServerError } from '../../../errors';
+import { CustomClient } from '../../../custom-client';
 // import { UserModel } from '../../../db/models';
 import { Request, Response } from 'express';
-// import { config } from '../../../../config';
 require('express-async-errors')
 
 async function getBotGuilds(req: Request, res: Response): Promise<void> {
-    res.status(200).end()
+    const client: CustomClient = req.app.get('discordClient')
+
+    const guilds = client.guilds.cache.map((guild): GuildData => {
+        return {
+            name: guild.name,
+            icon: guild.iconURL(),
+            memberCount: guild.memberCount,
+            joinedTimestamp: guild.joinedTimestamp,
+            preferredLocale: guild.preferredLocale,
+        }
+    })
+
+    res.status(200).json({ guilds: guilds })
 }
 
 export { getBotGuilds }
