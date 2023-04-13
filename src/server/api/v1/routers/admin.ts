a4a866475d6209cfcf0f6b8e765c071b9f77e5be
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-13 08:45:49 -0700

    added userAuth middleware

--- src/server/api/v1/routers/admin.ts
+++ src/server/api/v1/routers/admin.ts
@@ -1,7 +1,10 @@
 import { getBotGuilds, getUsers } from '../controllers'
+import { userAuth } from '../middleware';
 import { Router } from 'express';
 
 const adminRouter: Router = Router();
+
+adminRouter.use('*', userAuth)
 
 adminRouter.route('/guilds').get(getBotGuilds)
 adminRouter.route('/users').get(getUsers)
