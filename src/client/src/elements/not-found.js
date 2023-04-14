fa8fa73469b2a5aa1701ae8a359a013c5f5c161c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-14 15:44:27 -0700

    dont import not-found.css cuz it doesnt exist

--- src/client/src/elements/not-found.js
+++ src/client/src/elements/not-found.js
@@ -1,9 +1,7 @@
-import '../css/not-found.css';
-
 export default function NotFound() {
-    return (<div class='not-found'>
+    return (<>
         <h1>Not Found</h1>
         <div class="break"></div>
         <h3><a href='/' class='gray-button'>Go back to home?</a></h3>
-    </div>)
+    </>)
 }
