683c58a5bc531a0f8d27e24eed40542706ea7ad2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-14 15:38:43 -0700

    improved notfound page

--- src/client/src/elements/not-found.js
+++ src/client/src/elements/not-found.js
@@ -1,5 +1,9 @@
+import '../css/not-found.css';
+
 export default function NotFound() {
-    return (<>
-        NOT FOUND
-    </>)
+    return (<div class='not-found'>
+        <h1>Not Found</h1>
+        <div class="break"></div>
+        <h3><a href='/' class='gray-button'>Go back to home?</a></h3>
+    </div>)
 }
