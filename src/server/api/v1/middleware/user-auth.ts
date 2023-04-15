abca928ce3b9a6e8774506b3e5eb5a58401c313d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-15 11:30:04 -0700

    removed admin check and changed error messages

--- src/server/api/v1/middleware/user-auth.ts
+++ src/server/api/v1/middleware/user-auth.ts
@@ -1,11 +1,11 @@
-import { ForbiddenError, UnauthorizedError } from '../../../errors';
 import { NextFunction, Request, Response } from 'express';
+import { UnauthorizedError } from '../../../errors';
 import { config } from '../../../../config'
 import jwt from 'jsonwebtoken'
 
 function userAuth(req: Request, res: Response, next: NextFunction): void {
 	if (!req.cookies.userID) {
-		throw new UnauthorizedError('Authenticate yourself.')
+		throw new UnauthorizedError("Please login.")
 	}
 
 	interface JwtPayload extends jwt.JwtPayload {
@@ -15,14 +15,10 @@ function userAuth(req: Request, res: Response, next: NextFunction): void {
 	const idJWT: string | JwtPayload = jwt.verify(req.cookies.userID, config.jwtSecret!)
 
 	if (!idJWT || typeof idJWT === 'string' || !idJWT.userID) {
-		throw new UnauthorizedError('Authenticate yourself.')
+		throw new UnauthorizedError("Please login.")
 	}
 
 	req.userID = idJWT.userID
-
-	if (!config.adminIDs.includes(req.userID)) {
-		throw new ForbiddenError('You are not an admin.')
-	}
 
 	next()
 }
