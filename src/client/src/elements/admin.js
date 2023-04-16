ec6e96040e6f64515dcdb4fbf188e6aaa452debd
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-16 09:17:22 -0700

    made progress

--- src/client/src/elements/admin.js
+++ src/client/src/elements/admin.js
@@ -3,27 +3,28 @@ import axios, * as others from 'axios'
 import { errorToast } from './toasts'
 
 export default function Admin() {
-	const [authorized, setAuthorized] = useState(false)
+	const [guilds, setGuilds] = useState([])
+	const [users, setUsers] = useState([])
 
-    useEffect(async () => {
-		const [guilds, users] = await Promise.all([
+    useEffect(() => {
+		Promise.all([
 			axios.get('/api/v1/admin/guilds'),
 			axios.get('/api/v1/admin/users')
 		])
-		.then(res => {
-			setAuthorized(true)
+		.then(([guildsRes, usersRes]) => {
+			console.log(guildsRes, usersRes)
+			setGuilds(guildsRes)
+			setUsers(usersRes)
 		})
 		.catch(err => {
-			setAuthorized(false)
 			errorToast(err.response.data.error || err.message)
 		});
-
-		console.log(guilds, users)
     }, [])
     
-	if (!authorized) {
-		return <h2>You're not allowed here!</h2>
-	} else {
-		return <h2>Authorized</h2>
-	}
+	return <>
+		<h1>Guilds: {guilds.length} Users: {users.length}</h1>
+		{ guilds.map(guild => {
+			return <h3>{guild.name}</h3>
+		})}
+	</>
 }
