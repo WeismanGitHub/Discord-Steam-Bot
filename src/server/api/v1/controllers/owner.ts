3278db089852834995909af285d724364bc25982
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-17 18:15:36 -0700

    created getOwners

--- src/server/api/v1/controllers/owner.ts
+++ src/server/api/v1/controllers/owner.ts
@@ -15,4 +15,16 @@ async function getAdmins(req: Request, res: Response): Promise<void> {
     res.status(200).json({ admins })
 }
 
-export { getAdmins }
+async function getOwners(req: Request, res: Response): Promise<void> {
+    const page = Number(req.query.page) || 0
+
+    if (!Number.isSafeInteger(page) || page < 0) {
+        throw new BadRequestError('Page is invalid.')
+    }
+
+    const owners = await UserModel.find({ level: 'owner' }).skip(page).limit(10).lean()
+
+    res.status(200).json({ owners })
+}
+
+export { getAdmins, getOwners }
