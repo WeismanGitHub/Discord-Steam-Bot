926bcf0a8a86455514a6c385147bfa6a2d77c672
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-17 18:35:30 -0700

    created v1Router

--- src/server/api/v1/routers/index.ts
+++ src/server/api/v1/routers/index.ts
@@ -1,7 +1,13 @@
 import adminRouter from './admin';
-import authRouter from "./auth";
+import ownerRouter from './owner';
+import authRouter from './auth';
 
-export {
-    authRouter,
-    adminRouter,
-}
+import { Router } from 'express';
+
+const v1Router: Router = Router();
+
+v1Router.use(adminRouter)
+v1Router.use(ownerRouter)
+v1Router.use(authRouter)
+
+export default v1Router
