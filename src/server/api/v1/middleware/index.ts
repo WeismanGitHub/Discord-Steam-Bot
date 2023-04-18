441fd4bba8193bc810c3fb2c8822682911926f26
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-17 17:50:53 -0700

    added ownerAuth

--- src/server/api/v1/middleware/index.ts
+++ src/server/api/v1/middleware/index.ts
@@ -1,7 +1,9 @@
 import adminAuth from './admin-auth'
+import ownerAuth from './owner-auth'
 import userAuth from './user-auth'
 
 export {
     userAuth,
-    adminAuth
+    adminAuth,
+    ownerAuth,
 }
