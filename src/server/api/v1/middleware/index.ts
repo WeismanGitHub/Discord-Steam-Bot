85f7d40c282d639efbc30dcf332ab8178d8e8575
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-18 00:52:38 -0700

    moved functions into index

--- src/server/api/v1/middleware/index.ts
+++ src/server/api/v1/middleware/index.ts
@@ -1,6 +1,80 @@
-import adminAuth from './admin-auth'
-import ownerAuth from './owner-auth'
-import userAuth from './user-auth'
+import { ForbiddenError, UnauthorizedError } from '../../../errors';
+import { NextFunction, Request, Response } from 'express';
+import { UserModel } from '../../../db/models';
+import { config } from '../../../../config'
+import jwt from 'jsonwebtoken'
+
+function userAuth(req: Request, res: Response, next: NextFunction): void {
+	if (!req.cookies.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	interface JwtPayload extends jwt.JwtPayload {
+		userID?: string;
+	}
+	
+	const idJWT: string | JwtPayload = jwt.verify(req.cookies.userID, config.jwtSecret!)
+
+	if (!idJWT || typeof idJWT === 'string' || !idJWT.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	req.userID = idJWT.userID
+
+	next()
+}
+
+async function adminAuth(req: Request, res: Response, next: NextFunction): Promise<void> {
+	if (!req.cookies.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	interface JwtPayload extends jwt.JwtPayload {
+		userID?: string;
+	}
+	
+	const idJWT: string | JwtPayload = jwt.verify(req.cookies.userID, config.jwtSecret!)
+
+	if (!idJWT || typeof idJWT === 'string' || !idJWT.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	const user = await UserModel.findById(idJWT.userID).lean()
+
+	if (!user || user.level == 'user') {
+		throw new ForbiddenError('You are not an admin or owner.')
+	}
+
+	req.user = user
+	
+	next()
+}
+
+async function ownerAuth(req: Request, res: Response, next: NextFunction): Promise<void> {
+	if (!req.cookies.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	interface JwtPayload extends jwt.JwtPayload {
+		userID?: string;
+	}
+	
+	const idJWT: string | JwtPayload = jwt.verify(req.cookies.userID, config.jwtSecret!)
+
+	if (!idJWT || typeof idJWT === 'string' || !idJWT.userID) {
+		throw new UnauthorizedError("Please login.")
+	}
+
+	const user = await UserModel.findById(idJWT.userID).lean()
+
+	if (!user || user.level == 'owner') {
+		throw new ForbiddenError('You are not an owner.')
+	}
+
+	req.user = user
+	
+	next()
+}
 
 export {
     userAuth,
