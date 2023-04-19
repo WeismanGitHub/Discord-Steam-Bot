f7135c473f176f19dad129a433285964e026fc44
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-18 21:19:13 -0700

    added navbar + other stuff

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,12 +1,32 @@
+import { errorToast } from './elements/toasts';
+import About from './elements/about';
 import { useState } from 'react'
+import axios from 'axios';
+import './css/main.css';
 
 export default function Home() {
-    const userData = localStorage.getItem('userData')
-    const loggedIn = Boolean(userData)
-    const level = userData?.level
+    const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')))
+    const [loggedIn, setLoggedIn] = useState(Boolean(userData))
 
-    console.log(loggedIn)
+    function logout() {
+        localStorage.removeItem('userData')
+        setUserData({})
+
+        axios.post('/api/v1/auth/logout')
+        .then(res => setLoggedIn(false))
+        .catch(err => errorToast('Could not log out.'))
+    }
+
     return (<>
-        { loggedIn ? 'logged in!' : 'not logged in' }
+        <div class='navbar'>
+            {['owner', 'admin'].includes(userData?.level) && <a class='navbar-button' href="/privileged">dashboard</a>}
+            {loggedIn && <a class='navbar-button' href="/about">about</a>}
+            {loggedIn ? <button class='navbar-button' onClick={logout}>logout</button> : <>
+                <a class='navbar-button' href="/auth/login">login</a>
+                <a class='navbar-button' href="/auth/discord">register</a>
+            </>}
+
+            {loggedIn || <About/>}
+        </div>
     </>)
 }
