5eae273fc269ef3682ceefca88a7a0471e5840cf
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 15:34:29 -0700

    made config a class

--- src/config.ts
+++ src/config.ts
@@ -1,43 +1,69 @@
 import { InternalServerError } from "./server/errors";
 require("dotenv").config();
 
-const config: configuration = {
-    // Discord
-    discordToken: process.env.DISCORD_TOKEN,
-    testGuildID: process.env.TEST_GUILD_ID,
-    discordClientID: process.env.DISCORD_CLIENT_ID,
-    discordClientSecret: process.env.DISCORD_CLIENT_SECRET,
-    mainAccountID: process.env.DISCORD_MAIN_ACCOUNT_ID,
-    discordStatus: 'something',
+class Configuration {
+    discordToken: string
+    testGuildID: string
+    discordClientID: string
+    discordClientSecret: string
+    mainAccountID: string
+    discordStatus: string
+    appPort: number
+    jwtSecret: string
+    authRedirectURI: string
+    loginRedirectURI: string
+    buyMeACofeeLink: string
+    sourceCodeLink: string
+    steamAPIKey: string
+    mongoURI: string
+    mongoMaxPoolSize: number
+    mongoWtimeoutMS: number
+    limiterWindowMs: number
+    limiterMax: number
+    limiterMessage: string
+    limiterStandardHeaders: boolean
+    limiterLegacyHeaders: boolean
+    
+    constructor() {
+        // Discord
+        this.discordToken = process.env.DISCORD_TOKEN!
+        this.testGuildID = process.env.TEST_GUILD_ID!
+        this.discordClientID = process.env.DISCORD_CLIENT_ID!
+        this.discordClientSecret = process.env.DISCORD_CLIENT_SECRET!
+        this.mainAccountID = process.env.DISCORD_MAIN_ACCOUNT_ID!
+        this.discordStatus = 'something'
 
-    // App
-    appPort: 5000,
-    jwtSecret: process.env.JWT_SECRET,
-    authRedirectURI: process.env.AUTH_REDIRECT_URI,
-    loginRedirectURI: process.env.LOGIN_REDIRECT_URI,
-    buyMeACofeeLink: process.env.BUY_ME_A_COFFEE_LINK,
-    sourceCodeLink: process.env.SOURCE_CODE_LINK,
-    steamAPIKey: process.env.STEAM_API_KEY,
+        // App
+        this.appPort = 5000
+        this.jwtSecret = process.env.JWT_SECRET!
+        this.authRedirectURI = process.env.AUTH_REDIRECT_URI!
+        this.loginRedirectURI = process.env.LOGIN_REDIRECT_URI!
+        this.buyMeACofeeLink = process.env.BUY_ME_A_COFFEE_LINK!
+        this.sourceCodeLink = process.env.SOURCE_CODE_LINK!
+        this.steamAPIKey = process.env.STEAM_API_KEY!
 
-    // Mongodb
-    mongoURI: process.env.MONGO_URI,
-    mongoMaxPoolSize: 50,
-    mongoWtimeoutMS: 2500,
+        // Mongodb
+        this.mongoURI = process.env.MONGO_URI!
+        this.mongoMaxPoolSize = 50
+        this.mongoWtimeoutMS = 2500
 
-    // Rate Limiter
-    limiterWindowMs: 2000,
-    limiterMax: 30,
-    limiterMessage: 'Rate Limit: 30 requests per two seconds.',
-    limiterStandardHeaders: true,
-    limiterLegacyHeaders: false,
-}
+        // Rate Limiter
+        this.limiterWindowMs = 2000
+        this.limiterMax = 30
+        this.limiterMessage = 'Rate Limit = 30 requests per two seconds.'
+        this.limiterStandardHeaders = true
+        this.limiterLegacyHeaders = false
 
-for (const entry of Object.entries(config)) {
-    const [key, value] = entry
-
-    if (Number.isNaN(value) || value === undefined) {
-        throw new InternalServerError(`${key} is missing.`)
+        for (const entry of Object.entries(this)) {
+            const [key, value] = entry
+        
+            if (Number.isNaN(value) || value === undefined) {
+                throw new InternalServerError(`${key} is missing.`)
+            }
+        }
     }
 }
 
-export { config }
+const Config = new Configuration()
+
+export { Config }
