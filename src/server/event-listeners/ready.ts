673c2a7a0fbab623f5f61c4320b23b2d73873d86
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 15:41:43 -0700

    set activity elsewere

--- src/server/event-listeners/ready.ts
+++ src/server/event-listeners/ready.ts
@@ -1,12 +1,10 @@
 import { CustomClient } from "../custom-client";
 const { Events } = require('discord.js');
-import { Config } from '../../config'
 
 export default {
 	name: Events.ClientReady,
 	once: true,
 	execute(client: CustomClient) {
         console.log('bot is ready...')
-        client.setPresence(Config.activityType, Config.discordStatus)
 	},
 };
