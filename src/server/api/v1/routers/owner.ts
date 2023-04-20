8a04898fd8d07a47ee67e419b95d124cc260e51e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 16:02:29 -0700

    added restart/kill process routes

--- src/server/api/v1/routers/owner.ts
+++ src/server/api/v1/routers/owner.ts
@@ -1,12 +1,18 @@
-import { getOwners, getAdmins } from '../controllers/owner'
 import { ownerAuth } from '../middleware';
 import { Router } from 'express';
+import {
+    getOwners,
+    getAdmins,
+    killProcess,
+    restartProcess,
+} from '../controllers/owner'
 
 const ownerRouter: Router = Router();
 
-ownerRouter.use('*', ownerAuth)
+ownerRouter.get('/admins', ownerAuth, getAdmins)
+ownerRouter.get('/owners', ownerAuth, getOwners)
 
-ownerRouter.get('/admins', getAdmins)
-ownerRouter.get('/owners', getOwners)
+ownerRouter.post('/process/kill', ownerAuth, killProcess)
+ownerRouter.post('/process/restart', ownerAuth, restartProcess)
 
 export default ownerRouter
