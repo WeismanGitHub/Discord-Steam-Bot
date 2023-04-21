c50e501ab9a3c1d4cfacbdace0848a614e431ae8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 19:38:40 -0700

    get userData

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
 import NavBar from './elements/nav-bar';
 import About from './elements/about';
 
 export default function Home() {
+    const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')))
+    const [loggedIn, setLoggedIn] = useState(Boolean(userData))
+    
     return (<>
         <NavBar/>
         {loggedIn || <About/>}
