8413e050cf9e0898d12a32ebe89c1faad05f6a52
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 17:46:21 -0700

    changed response json

--- src/server/api/v1/controllers/admin.ts
+++ src/server/api/v1/controllers/admin.ts
@@ -18,7 +18,7 @@ async function getBotGuilds(req: Request, res: Response): Promise<void> {
         }
     })
 
-    res.status(200).json(guilds)
+    res.status(200).json({ guilds })
 }
 
 async function getUsers(req: Request, res: Response): Promise<void> {
@@ -47,7 +47,7 @@ async function getUsers(req: Request, res: Response): Promise<void> {
         }
     }))
 
-    res.status(200).json(users)
+    res.status(200).json({ users })
 }
 
 async function getBotData(req: Request, res: Response): Promise<void> {
