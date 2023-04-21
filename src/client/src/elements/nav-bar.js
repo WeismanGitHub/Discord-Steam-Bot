ed02cf6e81ac1348968931d70b4d9652906ff78c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 17:38:35 -0700

    created navbar

---
+++ src/client/src/elements/nav-bar.js
@@ -0,0 +1,31 @@
+import { errorToast } from './elements/toasts';
+import About from './elements/about';
+import { useState } from 'react'
+import axios from 'axios';
+import './css/main.css';
+
+export default function NavBar() {
+    const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')))
+    const [loggedIn, setLoggedIn] = useState(Boolean(userData))
+
+    function logout() {
+        axios.post('/api/v1/auth/logout')
+        .then(res => {
+            setLoggedIn(false)
+            localStorage.removeItem('userData')
+            setUserData({})
+        })
+        .catch(err => errorToast('Could not log out.'))
+    }
+
+    return (<>
+        <div class='navbar'>
+            {['owner', 'admin'].includes(userData?.level) && <a class='navbar-button' href="/privileged">dashboard</a>}
+            {loggedIn && <a class='navbar-button' href="/about">about</a>}
+            {loggedIn ? <button class='navbar-button' onClick={logout}>logout</button> : <>
+                <a class='navbar-button' href="/auth/login">login</a>
+                <a class='navbar-button' href="/auth/discord">register</a>
+            </>}
+        </div>
+    </>)
+}
