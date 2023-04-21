c1e3d8634d5b0a103a0819dd55eb66c1ff1124d7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-20 22:58:03 -0700

    created setStatus

--- src/server/api/v1/controllers/owner.ts
+++ src/server/api/v1/controllers/owner.ts
@@ -1,6 +1,7 @@
 import { BadRequestError } from '../../../errors';
 import { UserModel } from '../../../db/models';
 import { Request, Response } from 'express';
+import { ActivityType } from 'discord.js';
 require('express-async-errors')
 
 async function getAdmins(req: Request, res: Response): Promise<void> {
@@ -28,17 +29,34 @@ async function getOwners(req: Request, res: Response): Promise<void> {
 }
 
 function killProcess(req: Request, res: Response): void {
-    res.status(202)
+    res.status(202).end()
     process.exit(1)
 }
 
 function restartProcess(req: Request, res: Response): void {
-    res.status(202)
+    res.status(202).end()
+}
+
+async function setStatus(req: Request, res: Response): Promise<void> {
+    const { name, type }: { name: string | undefined, type: ActivityType | undefined } = req.body
+
+    if (!name || !type) {
+        throw new BadRequestError('Missing name or type.')
+    }
+
+    if (name.length > 50) {
+        throw new BadRequestError('Name must be less than 50 characters.')
+    }
+
+    // write name + type to activity.json
+
+    res.status(200).end()
 }
 
 export {
     getAdmins,
     getOwners,
-    killProcess, 
-    restartProcess
+    killProcess,
+    restartProcess,
+    setStatus,
 }
