c5290e2745d388f05db4ceb1645df57af709c80e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-22 01:27:33 -0700

    implemented buttons to get user pages

--- src/client/src/elements/privileged.js
+++ src/client/src/elements/privileged.js
@@ -1,13 +1,14 @@
+import { errorToast, successToast } from '../toasts'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import Navbar from '../elements/nav-bar';
 import axios, * as others from 'axios'
-import { errorToast, successToast } from '../toasts'
 import '../css/Privileged.css';
 
 export default function Privileged() {
 	const userData = localStorage.getItem('userData')
 	const [botData, setBotData] = useState(null)
+	const [userPage, setUserPage] = useState(0)
 	const [guilds, setGuilds] = useState([])
 	const [users, setUsers] = useState([])
 	const navigate = useNavigate();
@@ -18,15 +19,34 @@ export default function Privileged() {
 	function stopProcess() {
 		axios.post('/api/v1/owner/process/kill')
 		.then(res => successToast('Request was acknowledged.'))
-		.catch(err => {
-			errorToast(err.response.data.error || err.message)
-		});
+		.catch(err => errorToast(err?.response?.data?.error || err.message));
 	}
 
 	function restartProcess() {
 		axios.post('/api/v1/owner/process/restart')
 		.then(res => successToast('Request was acknowledged.'))
-		.catch(err => errorToast(err.response.data.error || err.message));
+		.catch(err => errorToast(err?.response?.data?.error || err.message));
+	}
+
+	function fetchUsers(page) {
+		if (page < 0) {
+			return errorToast('Cannot go below 1.')
+		}
+
+		if (users.length < 10) {
+			return errorToast('No more users left.')
+		}
+
+		axios.get(`/api/v1/admin/users?page=${page}`)
+		.then(res => {
+			if (!res?.data?.users) {
+				return errorToast('Something went wrong getting more users.')
+			}
+			
+			setUserPage(page)
+			setUsers(res.data.users)
+		})
+		.catch(err => errorToast(err?.response?.data?.error || err.message));
 	}
 	
 	useEffect(() => {
@@ -86,14 +106,19 @@ export default function Privileged() {
 				</div>
 			})}
 		</div>
-
+		
 		<div class='column' style={{ width: '20%' }}>
 			Users
+			<div class='next-before-buttons'>
+				<button onClick={() => fetchUsers(userPage - 1)}>Before</button>
+				{userPage + 1}
+				<button onClick={() => fetchUsers(userPage + 1)}>Next</button>
+			</div>
 			<hr class="divider"/>
 
 			{users?.map(user => {
 				return <div class='column-item' title={user.name}>
-					<img src={user.avatarURL} alt='user avatar' width={50} height={50} class='icon'/>
+					<img src={user.avatarURL} alt='user avatar' width={55} height={55} class='icon'/>
 					<div class='name'>{user.name}</div>
 				</div>
 			})}
