51096b0f4ec79aa417a422ce9461aff60409cc1c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-22 03:33:56 -0700

    implemented people type buttons

--- src/client/src/elements/privileged.js
+++ src/client/src/elements/privileged.js
@@ -6,15 +6,41 @@ import axios, * as others from 'axios'
 import '../css/Privileged.css';
 
 export default function Privileged() {
+	const [personType, setPersonType] = useState(localStorage.getItem('personType') || 'users')
 	const userData = localStorage.getItem('userData')
+	const [peoplePage, setPeoplePage] = useState(0)
 	const [botData, setBotData] = useState(null)
-	const [userPage, setUserPage] = useState(0)
 	const [guilds, setGuilds] = useState([])
-	const [users, setUsers] = useState([])
+	const [people, setPeople] = useState([])
 	const navigate = useNavigate();
 
 	const [admins, setAdmins] = useState([])
 	const [owners, setOwners] = useState([])
+	
+	useEffect(() => {
+		if (!userData || userData.level == 'user') {
+			errorToast('You must be an admin or owner.')
+			return navigate('/')
+		}
+
+		Promise.all([
+			axios.get('/api/v1/admin/guilds'),
+			axios.get(`/api/v1/${personType === 'users' ? 'admin' : 'owner'}/${personType}`),
+			axios.get('/api/v1/admin/bot'),
+			userData.level == 'owner' ? axios.get('/api/v1/owner/admins') : null,
+			userData.level == 'owner' ? axios.get('/api/v1/owner/owners') : null,
+		])
+		.then(([guildsRes, usersRes, botRes, adminsRes, ownersRes]) => {
+			setGuilds(guildsRes.data.guilds)
+			setPeople(usersRes.data)
+			setBotData(botRes.data)
+			setAdmins(adminsRes?.admins)
+			setOwners(ownersRes?.owners)
+		})
+		.catch(err => {
+			errorToast(err?.response?.data?.error || err.message)
+		});
+    }, [])
 
 	function stopProcess() {
 		axios.post('/api/v1/owner/process/kill')
@@ -28,51 +54,32 @@ export default function Privileged() {
 		.catch(err => errorToast(err?.response?.data?.error || err.message));
 	}
 
-	function fetchUsers(page) {
+	function fetchPeople(page, type=personType) {
 		if (page < 0) {
 			return errorToast('Cannot go below 1.')
 		}
 
-		if (users.length < 10) {
-			return errorToast('No more users left.')
+		if (people.length < 10) {
+			return errorToast('No more people left.')
 		}
 
-		axios.get(`/api/v1/admin/users?page=${page}`)
+		axios.get(`/api/v1/${type === 'users' ? 'admin' : 'owner'}/${type}`)
 		.then(res => {
-			if (!res?.data?.users) {
-				return errorToast('Something went wrong getting more users.')
+			if (!res?.data) {
+				return errorToast('Something went wrong getting more people.')
 			}
 			
-			setUserPage(page)
-			setUsers(res.data.users)
+			setPeoplePage(page)
+			setPeople(res.data)
 		})
 		.catch(err => errorToast(err?.response?.data?.error || err.message));
 	}
-	
-	useEffect(() => {
-		if (!userData || userData.level == 'user') {
-			errorToast('You must be an admin or owner.')
-			return navigate('/')
-		}
 
-		Promise.all([
-			axios.get('/api/v1/admin/guilds'),
-			axios.get('/api/v1/admin/users'),
-			axios.get('/api/v1/admin/bot'),
-			userData.level == 'owner' ? axios.get('/api/v1/owner/admins') : null,
-			userData.level == 'owner' ? axios.get('/api/v1/owner/owners') : null,
-		])
-		.then(([guildsRes, usersRes, botRes, adminsRes, ownersRes]) => {
-			setGuilds(guildsRes.data.guilds)
-			setUsers(usersRes.data.users)
-			setBotData(botRes.data)
-			setAdmins(adminsRes?.admins)
-			setOwners(ownersRes?.owners)
-		})
-		.catch(err => {
-			errorToast(err?.response?.data?.error || err.message)
-		});
-    }, [])
+	function personTypeClick(type) {
+		localStorage.setItem('personType', type)
+		setPersonType(type)
+		fetchPeople(0, type)
+	}
 
 	return <>
 		<Navbar/>
@@ -108,18 +115,23 @@ export default function Privileged() {
 		</div>
 		
 		<div class='column' style={{ width: '20%' }}>
-			Users
+				{userData?.level == 'admin' ? 'Users' :
+				<div>
+					<button class='people-type-button' onClick={() => personTypeClick('users')}>Users</button>
+					<button class='people-type-button' onClick={() => personTypeClick('admins')}>Admins</button>
+					<button class='people-type-button' onClick={() => personTypeClick('owners')}>Owners</button>
+				</div>}
 			<div>
-				<button class='pagination-button' onClick={() => fetchUsers(userPage - 1)}>Before</button>
-				{userPage + 1}
-				<button class='pagination-button' onClick={() => fetchUsers(userPage + 1)}>Next</button>
+				<button class='pagination-button' onClick={() => fetchPeople(peoplePage - 1)}>Before</button>
+				{peoplePage + 1}
+				<button class='pagination-button' onClick={() => fetchPeople(peoplePage + 1)}>Next</button>
 			</div>
 			<hr class="divider"/>
 
-			{users?.map(user => {
-				return <div class='column-item' title={user.name}>
-					<img src={user.avatarURL} alt='user avatar' width={54} height={54} class='icon'/>
-					<div class='name'>{user.name}</div>
+			{people?.map(person => {
+				return <div class='column-item' title={person.name}>
+					<img src={person.avatarURL} alt='person avatar' width={50} height={50} class='icon'/>
+					<div class='name'>{person.name}</div>
 				</div>
 			})}
 		</div>
