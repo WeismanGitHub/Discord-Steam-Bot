eda1412cc0eeaacb27fa66d152b5e437629c07ef
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-26 22:39:44 -0700

    created getSteamLevel

--- src/server/utils/steam-queries.ts
+++ src/server/utils/steam-queries.ts
@@ -5,6 +5,11 @@ async function getPlayerSummaries(steamID: string): Promise<player | undefined> 
     return (await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Config.steamAPIKey}&steamids=${steamID}`))?.data?.response?.players?.[0]
 }
 
+async function getSteamLevel(steamID: string): Promise<number | undefined> {
+    return (await axios.get(`https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=${Config.steamAPIKey}&steamid=${steamID}`)).data?.response?.player_level
+}
+
 export {
-    getPlayerSummaries
+    getPlayerSummaries,
+    getSteamLevel,
 }
