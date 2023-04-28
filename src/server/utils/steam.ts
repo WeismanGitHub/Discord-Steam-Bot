66b86623da75c0cc862c54fde5cb532877cf6aa9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-27 21:32:20 -0700

    extended getPlayerSummaries

--- src/server/utils/steam.ts
+++ src/server/utils/steam.ts
@@ -1,8 +1,10 @@
 import { Config } from '../../config'
 import axios, * as _ from 'axios'
 
-async function getPlayerSummaries(steamID: string): Promise<player | undefined> {
-    return (await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Config.steamAPIKey}&steamids=${steamID}`))?.data?.response?.players?.[0]
+async function getPlayerSummaries(steamIDs: string | string[]): Promise<player | undefined> {
+    steamIDs = Array.isArray(steamIDs) ? steamIDs.join(',') : steamIDs
+
+    return (await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Config.steamAPIKey}&steamids=${steamIDs}`))?.data?.response?.players?.[0]
 }
 
 async function getSteamLevel(steamID: string): Promise<number | undefined> {
