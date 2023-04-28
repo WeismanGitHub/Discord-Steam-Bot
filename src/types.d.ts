691081e7bbcd4a49abf94b098bf69fb528672f8b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-27 22:13:13 -0700

    removed wishlistItem

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,41 +10,6 @@ interface connection {
     metadata_visibility: number
 }
 
-// Wishlist items are from an outside API so you can't really trust it'll be consistent.
-interface wishlistItem {
-    name: undefined | string
-    capsule: undefined | string
-    review_score: undefined | number
-    review_desc: undefined | 'Overwhelmingly Positive' | 'Very Positive' | 'Positive' | 'Mostly Positive' | 'Mixed' |'Mostly Negative' | 'Negative' | 'Very Negative' | 'Overwhelmingly Negative' | 'No user reviews'
-    reviews_total: undefined | string
-    reviews_percent: number | undefined
-    release_date: string | undefined
-    release_string: string | undefined
-    platform_icons: string | undefined
-    subs: wishlistSub[] | undefined
-    type: string | undefined
-    screenshots: string[] | undefined
-    review_css: string | undefined
-    priority: number | undefined
-    added: number | undefined
-    background: string | undefined
-    rank: number | undefined
-    tags: string[] | undefined
-    is_free_game: boolean | undefined
-    deck_compat: string | undefined
-    early_access: number | undefined
-    win: number | undefined
-    mac: number | undefined
-    linux: number | undefined
-}
-
-interface wishlistSub {
-    id: number | undefiend
-    discount_block: string | undefined
-    discount_pct: number | undefined
-    price: number | undefined
-}
-
 interface User extends Document {
     _id: string
     steamID: string,
