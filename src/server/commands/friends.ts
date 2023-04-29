9a08b3841e57a593b2e4138d0c0eeda0fbcb86dd
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-29 00:21:20 -0700

    finished

--- src/server/commands/friends.ts
+++ src/server/commands/friends.ts
@@ -1,17 +1,13 @@
-import { BadGatewayError, BadRequestError } from '../errors';
+import { getFriendsList, getPlayerSummaries } from '../utils/steam';
+import { BadRequestError, InternalServerError } from '../errors';
 import { UserModel } from '../db/models';
-import { Config } from '../../config';
-import axios, * as _ from 'axios'
 import {
     SlashCommandBuilder,
     ChatInputCommandInteraction,
     User,
     EmbedBuilder,
-    ChannelType,
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
 } from 'discord.js'
+import { titleEmbed } from '../utils/embeds';
 
 export default {
 	data: new SlashCommandBuilder()
@@ -37,95 +33,36 @@ export default {
             throw new BadRequestError('User is not in database.')
         }
 
-        const res = await axios.get(`
-            http://api.steampowered.com/ISteamUser/GetFriendList/v0001/
-            ?key=${Config.steamAPIKey}
-            &steamid=${steamID}
-            &relationship=friend
-        `)
-        .catch(err => {
-            throw new BadGatewayError('Error getting friends list.')
-        })
+        const friends = await getFriendsList(steamID)
 
-        console.log(res)
+        if (!friends) {
+            throw new InternalServerError('Could not get friends.')
+        }
+        
+        const friendsProfiles = await getPlayerSummaries(friends.map(friend => friend.steamid))
 
-        return
-        let wishlistItems: wishlistItem[] = Object.values(res.data)
-
-        if (!wishlistItems.length) {
-            throw new BadRequestError('User has empty wishlist.')
+        if (!friendsProfiles?.length) {
+            throw new BadRequestError('User has no friends.')
         }
 
-        const reviewsOption = interaction.options.getString('reviews')
-        const freeOption = interaction.options.getBoolean('free')
-
-        if (freeOption !== null) {
-            wishlistItems = wishlistItems.filter((item): boolean => Boolean(item.is_free_game) === freeOption)
-        }
-
-        if (reviewsOption) {
-            wishlistItems = wishlistItems.filter((item): boolean => item.review_desc === reviewsOption)
-        }
-
-        const wishlistEmbeds: EmbedBuilder[] = wishlistItems.map((item): EmbedBuilder => {
+        const friendsEmbeds = friendsProfiles.map((friend): EmbedBuilder => {
             return new EmbedBuilder()
-            .setTitle(item.name || 'Missing Title')
-            .setImage(item.capsule || item.background || null)
-            .setFooter({ text: `Release: ${item.release_string || 'unknown'}` })
-            .setColor('#8F00FF') // Purple
-            .addFields({
-                name: 'Reviews:',
-                value: item.reviews_percent ? `${item.reviews_percent}% positive` : 'unknown',
-                inline: false
-            })
-            .addFields({
-                name: 'Free:',
-                value: String(Boolean(item.is_free_game)),
-                inline: false
-            })
-            .addFields({
-                name: 'Tags:',
-                value: item.tags ? item.tags.join(', ') : 'none',
-                inline: false
-            })
+            .setTitle(friend.personaname || 'Missing Name')
         })
-
-        if (!wishlistEmbeds.length) {
-            throw new BadRequestError('No wishlist items found.')
-        }
 
         const embedGroups = [];
 
-        while (wishlistEmbeds.length > 0) {
-            embedGroups.push(wishlistEmbeds.splice(0, 10))
-        }
-
-        if (interaction.channel?.type !== ChannelType.GuildText) {
-            return
+        while (friendsEmbeds.length > 0) {
+            embedGroups.push(friendsEmbeds.splice(0, 10))
         }
 
         await interaction.reply({ embeds: embedGroups[0], ephemeral: true })
 
         await Promise.all(embedGroups.slice(1).map((embedGroup) => interaction.followUp({ embeds: embedGroup, ephemeral: true })))
 
-        const row = new ActionRowBuilder<ButtonBuilder>()
-        .addComponents(
-            new ButtonBuilder()
-            .setLabel(`Next Page ⏩`)
-            .setCustomId(JSON.stringify({
-                type: 'wishlist',
-                data: {
-                    page: 0,
-                    steamID: 'user.steamID',
-                    filters: {
-                        free: freeOption,
-                        reviews: reviewsOption
-                    }
-                }
-            }))
-            .setStyle(ButtonStyle.Primary)
-        )
-
-        interaction.followUp({ components: [row], ephemeral: true })
+        interaction.followUp({
+            embeds: [titleEmbed(`Friend Count: ${friends.length ?? 'unknown'}`)],
+            ephemeral: true
+        })
     },
 };
