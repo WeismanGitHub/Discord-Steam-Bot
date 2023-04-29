e73e68113f58d08be85182d5909ca6855980ed4c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-29 05:06:43 -0700

    added demote/promote/ban users routes

--- src/server/api/v1/routers/admin.ts
+++ src/server/api/v1/routers/admin.ts
@@ -1,11 +1,21 @@
-import { getBot, getBotGuilds, getUsers } from '../controllers/admin'
-import { adminAuth } from '../middleware/auth';
 import { Router } from 'express';
+import {
+    banUser,
+    getBot,
+    getBotGuilds,
+    getUsers,
+    promoteUser,
+    demoteUser,
+} from '../controllers/admin'
 
 const adminRouter: Router = Router();
 
-adminRouter.get('/guilds', adminAuth, getBotGuilds)
-adminRouter.get('/users', adminAuth, getUsers)
-adminRouter.get('/bot', adminAuth, getBot)
+adminRouter.get('/guilds', getBotGuilds)
+adminRouter.get('/bot', getBot)
+
+adminRouter.get('/users', getUsers)
+adminRouter.post('/users/:userID/ban', banUser)
+adminRouter.post('/users/:userID/promote', promoteUser)
+adminRouter.post('/users/:userID/demote', demoteUser)
 
 export default adminRouter
