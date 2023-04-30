2ef4757df7999c2006ce0b907e862ef9f0bdcc88
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-04-29 19:52:06 -0700

    created get user route

--- src/server/api/v1/routers/admin.ts
+++ src/server/api/v1/routers/admin.ts
@@ -6,6 +6,7 @@ import {
     getUsers,
     promoteUser,
     demoteUser,
+    getUser,
 } from '../controllers/admin'
 
 const adminRouter: Router = Router();
@@ -14,6 +15,7 @@ adminRouter.get('/guilds', getBotGuilds)
 adminRouter.get('/bot', getBot)
 
 adminRouter.get('/users', getUsers)
+adminRouter.get('/users/:userID', getUser)
 adminRouter.post('/users/:userID/ban', banUser)
 adminRouter.post('/users/:userID/promote', promoteUser)
 adminRouter.post('/users/:userID/demote', demoteUser)
