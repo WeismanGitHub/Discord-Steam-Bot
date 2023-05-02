88c2f8d54da5aab1412d8f2161d0237ac81e525a
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-02 01:15:33 -0700

    made progress

--- src/client/src/elements/account.js
+++ src/client/src/elements/account.js
@@ -7,6 +7,9 @@ import '../css/account.css';
 
 export default function Account() {
     const userData = JSON.parse(localStorage.getItem('userData'))
+	const [discordData, setDiscordData] = useState(null)
+	const [steamData, setSteamData] = useState(null)
+	const [type, setType] = useState(null)
 	const navigate = useNavigate();
 
     useEffect(() => {
@@ -19,14 +22,24 @@ export default function Account() {
 			axios.get('/api/v1/user/self')
 		])
 		.then(([selfRes]) => {
-			console.log(selfRes?.data)
+			setDiscordData(selfRes?.data.discord)
+			setSteamData(selfRes?.data.steam)
+			setType(selfRes?.data.type)
 		})
 		.catch(err => {
 			errorToast(err?.response?.data?.error || err.message)
 		});
     }, [])
-    
+
+	console.log(type, steamData, discordData)
     return (<>
         <NavBar/>
+		{/* <div class='column' style={{ width: '20%' }}>
+
+		</div>
+		<div class='column-item' title={person.name}>
+			<img src={person.avatarURL} alt='person avatar' width={53} height={53} class='icon'/>
+			<div class='name'>{person.name}</div>
+		</div> */}
     </>)
 }
