aa1f90f295a2055aefd5da5282c08ba3d26e5469
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-01 18:20:03 -0700

    improved description

--- src/client/src/elements/about.js
+++ src/client/src/elements/about.js
@@ -2,7 +2,7 @@ export default function About() {
     return (<>
         <h1>Discord Steam App</h1>
         <div style={{ width: '50%', margin: 'auto' }}>
-            Use slash commands to conveniently view other users' public Steam information. Allow access to view your connected Steam account through <a href='/auth/discord-auth'>this link</a> so others can use commands on you. Use `/unauthorize` to delete all data stored by the bot.
+            Use slash commands to conveniently view other users' public Steam information. Allow access to view your connected Steam account through <a href='/auth/discord'>this link</a> so others can use commands on you. Use `/unauthorize` to delete all data stored by the bot or visit <a href='/account'>the account page</a>.
         </div>
 
         <h4><a href={process.env.REACT_APP_BOT_INVITE_LINK}>Bot Invite</a></h4>
