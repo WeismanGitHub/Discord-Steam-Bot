e2246c7b830362195f587d4d8d9c56a345549626
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-02 00:49:28 -0700

    send a call to api for user data

--- src/client/src/elements/account.js
+++ src/client/src/elements/account.js
@@ -1,6 +1,7 @@
 import { errorToast, successToast } from '../toasts'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
+import axios, * as others from 'axios'
 import NavBar from './nav-bar';
 import '../css/account.css';
 
@@ -9,14 +10,16 @@ export default function Account() {
 	const navigate = useNavigate();
 
     useEffect(() => {
-		if (!userData || userData.type == 'user') {
-			errorToast('Please login first.')
+		if (!userData) {
+			errorToast('Please login.')
 			return navigate('/')
 		}
 
 		Promise.all([
+			axios.get('/api/v1/user/self')
 		])
-		.then(([]) => {
+		.then(([selfRes]) => {
+			console.log(selfRes?.data)
 		})
 		.catch(err => {
 			errorToast(err?.response?.data?.error || err.message)
