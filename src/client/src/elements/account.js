8aaac9ed2a0e899278f4896caa283a14927e94ed
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-01 18:26:56 -0700

    made progress

--- src/client/src/elements/account.js
+++ src/client/src/elements/account.js
@@ -1,7 +1,28 @@
+import { errorToast, successToast } from '../toasts'
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import NavBar from './nav-bar';
 import '../css/account.css';
 
 export default function Account() {
+    const userData = JSON.parse(localStorage.getItem('userData'))
+	const navigate = useNavigate();
+
+    useEffect(() => {
+		if (!userData || userData.type == 'user') {
+			errorToast('Please login first.')
+			return navigate('/')
+		}
+
+		Promise.all([
+		])
+		.then(([]) => {
+		})
+		.catch(err => {
+			errorToast(err?.response?.data?.error || err.message)
+		});
+    }, [])
+    
     return (<>
         <NavBar/>
     </>)
