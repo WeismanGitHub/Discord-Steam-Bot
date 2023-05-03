b25012bb3dab4574f305651132154ba77ed45b14
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-02 22:24:56 -0700

    added delete self route

--- src/server/api/v1/routers/user.ts
+++ src/server/api/v1/routers/user.ts
@@ -1,8 +1,9 @@
-import { getSelf } from '../controllers/user'
+import { deleteSelf, getSelf } from '../controllers/user'
 import { Router } from 'express';
 
 const userRouter: Router = Router();
 
 userRouter.get('/self', getSelf)
+userRouter.delete('/self', deleteSelf)
 
 export default userRouter
