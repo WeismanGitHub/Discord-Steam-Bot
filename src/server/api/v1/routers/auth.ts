7455fae60e116bf251438c5209a2c51f6d0b1756
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-02 22:33:26 -0700

    deleted unauthorize route

--- src/server/api/v1/routers/auth.ts
+++ src/server/api/v1/routers/auth.ts
@@ -1,17 +1,14 @@
-import { userAuth } from '../middleware/auth';
 import { Router } from 'express';
 import {
     discordAuth,
     logout,
-    unauthorize,
     login
 } from '../controllers/auth'
 
 const authRouter: Router = Router();
 
 authRouter.post('/login', login)
-authRouter.post('/discord', discordAuth)
 authRouter.post('/logout', logout)
-authRouter.post('/unauthorize', userAuth, unauthorize)
+authRouter.post('/discord', discordAuth)
 
 export default authRouter
