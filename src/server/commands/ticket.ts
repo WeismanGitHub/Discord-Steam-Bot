783ef1f9a08a67b1790a9c32252ff02ae5aca0d3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-04 02:13:55 -0700

    created ticket command

---
+++ src/server/commands/ticket.ts
@@ -0,0 +1,40 @@
+import {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder,
+} from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('ticket')
+		.setDescription("Create a ticket for anything you want to get through to the admins.")
+	,
+	async execute(interaction: ChatInputCommandInteraction) {
+        const modal = new ModalBuilder()
+        .setCustomId(JSON.stringify({ type: 'ticket', data: {} }))
+        .setTitle('Create a Ticket');
+
+        const titleInput = new TextInputBuilder()
+        .setCustomId('title')
+        .setLabel("Title")
+        .setStyle(TextInputStyle.Short)
+        .setMaxLength(256)
+        .setMinLength(1)
+
+        const bodyInput = new TextInputBuilder()
+        .setCustomId('body')
+        .setLabel("Body")
+        .setStyle(TextInputStyle.Paragraph)
+        .setMinLength(1)
+
+        modal.addComponents(
+            new ActionRowBuilder<TextInputBuilder>().addComponents(titleInput),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(bodyInput),
+        );
+
+        await interaction.showModal(modal);
+	},
+};
