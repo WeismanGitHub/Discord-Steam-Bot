f377a83ce661f5e3215e24883a521ea07025b402
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-04 03:16:35 -0700

    finished

--- src/server/event-listeners/modal-submit/ticket.ts
+++ src/server/event-listeners/modal-submit/ticket.ts
@@ -1,15 +1,29 @@
-import { ModalSubmitInteraction, Events } from "discord.js";
+import { TicketModel } from "../../db/models";
+import {
+    Events,
+    ModalSubmitInteraction
+} from "discord.js"
+import { titleEmbed } from "../../utils/embeds";
 
 export default {
 	name: Events.InteractionCreate,
-    once: false,
-	execute(interaction: ModalSubmitInteraction) {
+	once: false,
+    execute: async (interaction: ModalSubmitInteraction) => {
         if (!interaction.isModalSubmit()) return;
 
         const { type }: CustomID<{}> = JSON.parse(interaction.customId)
 
         if (type !== 'ticket') return
 
-        console.log(interaction.fields)
-	},
-};
+        const title = interaction.fields.getTextInputValue('text')
+        const text = interaction.fields.getTextInputValue('title')
+
+        await TicketModel.create({
+            title,
+            text,
+            userID: interaction.user.id
+        })
+
+        interaction.reply({ embeds: [titleEmbed('A ticket has been created.')] })
+    }
+}
