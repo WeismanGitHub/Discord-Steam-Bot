a75bf4c5c472c9cc4a3a6626da585bc5b7fc3998
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-04 20:25:24 -0700

    created

---
+++ src/server/api/v1/routers/ticket.ts
@@ -0,0 +1,17 @@
+// import { Router } from 'express';
+// import {
+//     getTickets
+// } from '../controllers/ticket'
+
+// const adminRouter: Router = Router();
+
+// adminRouter.get('/tickets', getTickets)
+// adminRouter.get('/bot', getBot)
+
+// adminRouter.get('/users', getUsers)
+// adminRouter.get('/users/banned', getBannedUsers)
+// adminRouter.get('/users/:userID', getUser)
+// adminRouter.post('/users/:userID/ban', banUser)
+// adminRouter.post('/users/:userID/unban', unbanUser)
+
+// export default adminRouter
