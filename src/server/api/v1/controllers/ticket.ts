aafea917d3505629b46aec11d6fe7d6fddf29cf1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-05 22:05:00 -0700

    created getTickets

---
+++ src/server/api/v1/controllers/ticket.ts
@@ -0,0 +1,34 @@
+import { BadGatewayError, BadRequestError, InternalServerError } from '../../../errors';
+import { TicketModel } from '../../../db/models';
+import { Request, Response } from 'express';
+require('express-async-errors')
+
+async function getTickets(req: Request, res: Response): Promise<void> {
+    const page = Number(req.query.page) || 0
+    const status = req.query.status ?? 'open'
+
+    if (!Number.isSafeInteger(page) || page < 0) {
+        throw new BadRequestError('Page is invalid.')
+    }
+
+    console.log(typeof TicketModel, status)
+
+    if (false) {
+        throw new BadGatewayError('Invalid status.')
+    }
+    
+    const tickets = (await TicketModel.find({ }).skip(page).limit(10).select('-text').lean()
+    .catch(err => {
+        throw new InternalServerError('Could not get user ids.')
+    })).map(user => user._id)
+
+    res.status(200).json(tickets)
+}
+
+async function getTicket(req: Request, res: Response): Promise<void> {
+}
+
+export {
+    getTickets,
+    getTicket,
+}
