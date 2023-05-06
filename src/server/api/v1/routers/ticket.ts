3419c88d748f1e9c7f13cbcfd6b70484ec9f30f6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-05 22:07:20 -0700

    added get ticket route

--- src/server/api/v1/routers/ticket.ts
+++ src/server/api/v1/routers/ticket.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import {
+    getTicket,
     getTickets
 } from '../controllers/ticket'
 
 const ticketRouter: Router = Router();
 
 ticketRouter.get('/', getTickets)
+ticketRouter.get('/:ticketID', getTicket)
 
 export default ticketRouter
