7fdeeb6d2e7c8d772d37325c1cf7e178262cbeb0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-10 20:13:35 -0700

    added validators

--- src/server/db/models/ticket.ts
+++ src/server/db/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import { BadRequestError } from '../../errors';
 
 interface Ticket extends Document {
     userID: string
@@ -18,13 +19,37 @@ const TicketSchema: Schema = new Schema<Ticket>({
         type: String,
         required: true,
         max: 256,
-        min: 1
+        min: 1,
+        validate: {
+            validator: function(text: string) {
+                if (text.length > 256) {
+                    throw new BadRequestError('Maximum length is 256.')
+                }
+
+                if (text.length < 1) {
+                    throw new BadRequestError('Minimum length is 1.')
+                }
+            },
+            message: props => `Invalid Input: \`${props.value}\``
+        },
     },
     text: {
         type: String,
         required: true,
         max: 4096,
-        min: 1
+        min: 1,
+        validate: {
+            validator: function(text: string) {
+                if (text.length > 4096) {
+                    throw new BadRequestError('Maximum length is 4096.')
+                }
+
+                if (text.length < 1) {
+                    throw new BadRequestError('Minimum length is 1.')
+                }
+            },
+            message: props => `Invalid Input: \`${props.value}\``
+        },
     },
     status: {
         type: String,
