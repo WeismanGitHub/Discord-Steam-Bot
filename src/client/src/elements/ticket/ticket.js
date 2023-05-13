61954e246f315e57dbe7221017b2804af0c119e9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-13 10:34:29 -0700

    display ticket data

--- src/client/src/elements/ticket/ticket.js
+++ src/client/src/elements/ticket/ticket.js
@@ -1,4 +1,26 @@
+import { errorToast, successToast } from '../../toasts'
+import { useEffect, useState } from "react";
+import axios, * as others from 'axios'
+import NavBar from '../nav-bar';
+
 export default function Ticket() {
+	const [ticket, setTicket] = useState(null)
+
+    useEffect(async () => {
+		const { data } = await axios.get(`/api/v1/tickets/${'64577108d0e3625b2749e657'}`)
+		.catch(err => errorToast(err?.response?.data?.error || err.message));
+
+        setTicket(data)
+	}, [])
+
     return (<>
+        <NavBar/>
+
+        <h2>{ticket?.title || 'title'}</h2>
+        <h6>status: {ticket?.status || 'unknown'}</h6>
+
+        <h3>{ticket?.text || 'text'}</h3>
+
+        <h3>{ticket?.response}</h3>
     </>)
 }
