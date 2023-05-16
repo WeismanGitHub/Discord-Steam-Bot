a63fccf53faad02d57419b5015f877eb8a97dbad
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-16 15:47:32 -0700

    gave up on validation being part of schema because you cant really throw your own errors. at best you can give custom error messages

--- src/server/db/models/ticket.ts
+++ src/server/db/models/ticket.ts
@@ -1,5 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
-import { BadRequestError } from '../../errors';
+import { Document, Schema, model } from 'mongoose';
 
 interface Ticket extends Document {
     userID: string
@@ -45,33 +44,4 @@ const TicketSchema: Schema = new Schema<Ticket>({
     }
 });
 
-TicketSchema.pre('save', function(next) {
-    const text: string = this.text
-    const title: string = this.text
-    const response: string | null = this.text
-
-    // Title Check
-    if (title.length > 256) {
-        return next(new BadRequestError('Maximum title length is 256.'))
-    } else if (title.length < 1) {
-        return next(new BadRequestError('Minimum title length is 1.'))
-    }
-    
-    // Text Check
-    if (text.length > 4096) {
-        return next(new BadRequestError('Maximum text length is 4096.'))
-    } else if (text.length < 1) {
-        return next(new BadRequestError('Minimum text length is 1.'))
-    }
-    
-    // Response Check
-    if (response !== null && response.length > 4096) {
-        return next(new BadRequestError('Maximum response length is 4096.'))
-    } else if (response !== null && response.length < 1) {
-        return next(new BadRequestError('Minimum response length is 1.'))
-    }
-
-    next()
-});
-
-export default mongoose.model<Ticket>('tickets', TicketSchema)
+export default model<Ticket>('tickets', TicketSchema)
