ad14431d948433e1408c6bfcba7580fed92a84f9
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-16 16:39:21 -0700

    finished

--- src/server/event-listeners/button-clicks/recent-games.ts
+++ src/server/event-listeners/button-clicks/recent-games.ts
@@ -1,7 +1,7 @@
-import { ButtonInteraction, Events } from "discord.js"
-// import { infoEmbed, recentGameEmbed } from "../../utils/embeds"
-// import { getRecentlyPlayedGames } from "../../utils/steam"
-// import { InternalServerError } from "../../errors"
+import { ButtonInteraction, EmbedBuilder, Events } from "discord.js"
+import { infoEmbed, recentGameEmbed } from "../../utils/embeds"
+import { getRecentlyPlayedGames } from "../../utils/steam"
+import { InternalServerError } from "../../errors"
 
 export default {
 	name: Events.InteractionCreate,
@@ -18,6 +18,39 @@ export default {
             return
         }
 
-        console.log(steamID)
+        const recentGamesData = await getRecentlyPlayedGames(steamID)
+        const gameCount = recentGamesData?.total_count
+        const recentGames= recentGamesData?.games
+
+        if (!recentGamesData) {
+            throw new InternalServerError('Could not get recently played games.')
+        }
+        
+        if (!recentGames?.length) {
+            return interaction.reply({
+                embeds: [infoEmbed('User has no recently played games.')],
+                ephemeral: true
+            })
+        }
+
+        const recentGamesEmbeds: EmbedBuilder[] = recentGames.map((game) => recentGameEmbed(game))
+        
+        const embedGroups: EmbedBuilder[][] = [];
+
+        while (recentGamesEmbeds.length > 0) {
+            embedGroups.push(recentGamesEmbeds.splice(0, 10))
+        }
+
+        await interaction.reply({
+            embeds: embedGroups[0],
+            ephemeral: true
+        })
+
+        await Promise.all(embedGroups.slice(1).map((embedGroup) => interaction.followUp({ embeds: embedGroup, ephemeral: true })))
+
+        interaction.followUp({
+            embeds: [infoEmbed(`Recent Games Count: ${gameCount ?? 'unknown'}`)],
+            ephemeral: true
+        })
     }
 }
