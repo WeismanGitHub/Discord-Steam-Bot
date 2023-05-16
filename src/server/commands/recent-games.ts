655613f3e80420cb0a536eb9fb039e6e7512f44c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-16 16:18:34 -0700

    created

---
+++ src/server/commands/recent-games.ts
@@ -0,0 +1,81 @@
+import { BadRequestError, InternalServerError } from '../errors';
+import { infoEmbed, recentGameEmbed } from '../utils/embeds';
+import { getRecentlyPlayedGames } from '../utils/steam';
+import { UserModel } from '../db/models';
+import {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    User,
+    EmbedBuilder,
+} from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('recent-games')
+		.setDescription("Get someone's recently played games.")
+        .setDMPermission(false)
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription("The user you want to target.")
+            .setRequired(true)
+        )
+	,
+	async execute(interaction: ChatInputCommandInteraction) {
+        const user: User = interaction.options.getUser('user')!
+        
+        if (user.bot) {
+            throw new BadRequestError('User is a bot.')
+        }
+
+        const userDoc = await UserModel.findById(user.id).select('-_id steamID role').lean()
+        
+        if (!userDoc) {
+            return interaction.reply({
+                embeds: [infoEmbed('User is not in database.')],
+                ephemeral: true
+            })
+        }
+
+        if (userDoc.role === 'banned') {
+            return interaction.reply({
+                embeds: [infoEmbed('User is banned.')],
+                ephemeral: true
+            })
+        }
+
+        const recentGamesData = await getRecentlyPlayedGames(userDoc.steamID)
+        const gameCount = recentGamesData?.total_count
+        const recentGames= recentGamesData?.games
+
+        if (!recentGamesData) {
+            throw new InternalServerError('Could not get recently played games.')
+        }
+        
+        if (!recentGames?.length) {
+            return interaction.reply({
+                embeds: [infoEmbed('User has no recently played games.')],
+                ephemeral: true
+            })
+        }
+
+        const recentGamesEmbeds: EmbedBuilder[] = recentGames.map((game) => recentGameEmbed(game))
+        
+        const embedGroups: EmbedBuilder[][] = [];
+
+        while (recentGamesEmbeds.length > 0) {
+            embedGroups.push(recentGamesEmbeds.splice(0, 10))
+        }
+
+        await interaction.reply({
+            embeds: embedGroups[0],
+            ephemeral: true
+        })
+
+        await Promise.all(embedGroups.slice(1).map((embedGroup) => interaction.followUp({ embeds: embedGroup, ephemeral: true })))
+
+        interaction.followUp({
+            embeds: [infoEmbed(`Recent Games Count: ${gameCount ?? 'unknown'}`)],
+            ephemeral: true
+        })
+    },
+};
