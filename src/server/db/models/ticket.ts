503eb6c1120e583eeb8275f3bd90d4f17c5fe07f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-15 18:49:27 -0700

    finished validation

--- src/server/db/models/ticket.ts
+++ src/server/db/models/ticket.ts
@@ -20,36 +20,12 @@ const TicketSchema: Schema = new Schema<Ticket>({
         required: true,
         max: 256,
         min: 1,
-        validate: {
-            validator: function(text: string) {
-                if (text.length > 256) {
-                    throw new BadRequestError('Maximum length is 256.')
-                }
-
-                if (text.length < 1) {
-                    throw new BadRequestError('Minimum length is 1.')
-                }
-            },
-            message: props => `Invalid Input: \`${props.value}\``
-        },
     },
     text: {
         type: String,
         required: true,
         max: 4096,
         min: 1,
-        validate: {
-            validator: function(text: string) {
-                if (text.length > 4096) {
-                    throw new BadRequestError('Maximum length is 4096.')
-                }
-
-                if (text.length < 1) {
-                    throw new BadRequestError('Minimum length is 1.')
-                }
-            },
-            message: props => `Invalid Input: \`${props.value}\``
-        },
     },
     status: {
         type: String,
@@ -66,24 +42,36 @@ const TicketSchema: Schema = new Schema<Ticket>({
         default: null,
         max: 4096,
         min: 1,
-        // DOESNT WORK :(
-        // validate: {
-        //     validator: function(text: string) {
-        //         if (text === null) {
-        //             return
-        //         }
-
-        //         if (text.length > 4096) {
-        //             throw new BadRequestError('Maximum length is 4096.')
-        //         }
-
-        //         if (text.length < 1) {
-        //             throw new BadRequestError('Minimum length is 1.')
-        //         }
-        //     },
-        //     message: props => `Invalid Input: \`${props.value}\``
-        // },
     }
+});
+
+TicketSchema.pre('save', function(next) {
+    const text: string = this.text
+    const title: string = this.text
+    const response: string | null = this.text
+
+    // Title Check
+    if (title.length > 256) {
+        return next(new BadRequestError('Maximum title length is 256.'))
+    } else if (title.length < 1) {
+        return next(new BadRequestError('Minimum title length is 1.'))
+    }
+    
+    // Text Check
+    if (text.length > 4096) {
+        return next(new BadRequestError('Maximum text length is 4096.'))
+    } else if (text.length < 1) {
+        return next(new BadRequestError('Minimum text length is 1.'))
+    }
+    
+    // Response Check
+    if (response !== null && response.length > 4096) {
+        return next(new BadRequestError('Maximum response length is 4096.'))
+    } else if (response !== null && response.length < 1) {
+        return next(new BadRequestError('Minimum response length is 1.'))
+    }
+
+    next()
 });
 
 export default mongoose.model<Ticket>('tickets', TicketSchema)
