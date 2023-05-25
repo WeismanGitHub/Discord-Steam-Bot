d9cfe59fbe3efbb23ba8d68b171963ef2439da89
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-24 22:50:27 -0700

    tickets button only sends to /tickets

--- src/client/src/elements/nav-bar.js
+++ src/client/src/elements/nav-bar.js
@@ -8,6 +8,7 @@ export default function NavBar() {
     const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')))
     const [loggedIn, setLoggedIn] = useState(Boolean(userData))
     const navigate = useNavigate();
+    const userIsPrivileged = ['owner', 'admin'].includes(userData?.role)
 
     function logout() {
         if (!window.confirm('Are you sure you want to logout?')) {
@@ -27,8 +28,8 @@ export default function NavBar() {
     return (<>
         <div class='navbar'>
             <a class='navbar-button' href="/">home</a>
-            {['owner', 'admin'].includes(userData?.role) && <a class='navbar-button' href="/privileged">dashboard</a>}
-            {loggedIn && <a class='navbar-button' href={['owner', 'admin'].includes(userData?.role) ? '/tickets' : '/tickets/create'}>tickets</a>}
+            {userIsPrivileged && <a class='navbar-button' href="/privileged">dashboard</a>}
+            {userIsPrivileged && <a class='navbar-button' href='/tickets'>tickets</a>}
             {loggedIn && <a class='navbar-button' href="/account">account</a>}
             {loggedIn ? <div class='navbar-button' onClick={logout}>logout</div> : <>
                 {window.location.pathname !== '/auth/login' && <a class='navbar-button' href="/auth/login">login</a>}
