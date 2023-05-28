4692e678a6c431602294c6f76c125fad622ce645
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-05-27 21:09:45 -0700

    added posts button

--- src/client/src/elements/nav-bar.js
+++ src/client/src/elements/nav-bar.js
@@ -30,6 +30,7 @@ export default function NavBar() {
             <a class='navbar-button' href="/">home</a>
             {userIsPrivileged && <a class='navbar-button' href="/privileged">dashboard</a>}
             {userIsPrivileged && <a class='navbar-button' href='/tickets'>tickets</a>}
+            <a class='navbar-button' href='/posts'>posts</a>
             {loggedIn && <a class='navbar-button' href="/account">account</a>}
             {loggedIn ? <div class='navbar-button' onClick={logout}>logout</div> : <>
                 <a class='navbar-button' href="/auth/login">login</a>
