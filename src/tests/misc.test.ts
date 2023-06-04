3b85507f8120c3b27aa2dca9383638889eba279e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-04 12:33:39 -0700

    added rateliming test

--- src/tests/misc.test.ts
+++ src/tests/misc.test.ts
@@ -7,4 +7,11 @@ describe("Miscellaneous Tests", () => {
 
         expect(res.statusCode).toBe(404)
     });
+
+    it("should return status code 429 after sending 30 requests in 2 seconds.", async () => {
+        await Promise.all([...Array(30).keys()].map(i => request(app).get('/api/v1/nonexistant')))
+
+        const res = await request(app).get("/api/v1/nonexistant")
+        expect(res.statusCode).toBe(429)
+    });
 });
